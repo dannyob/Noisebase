322d9a472075ac06336670fa1bcff2c4a8312ef0
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2011-11-20 14:03:13 -0800

    Modified User Migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :comments
   has_many :badges
+
   acts_as_authentic
+  
+  mount_uploader :avatar, AvatarUploader 
   
   def display_name
     self.name || self.email
--- db/migrate/20111120215658_create_users.rb
+++ db/migrate/20111120215658_create_users.rb
@@ -1,11 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :login
-      t.string :email
-      t.integer :views_count
-      t.text :bio
-
+      t.string  :login
+      t.string  :email
+      t.integer :views_count, :default => 0
+      t.integer :excellence, :default => 0
+      t.string  :avatar
+      t.text    :bio 
       t.timestamps
     end
   end
