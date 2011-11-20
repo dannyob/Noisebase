97158a9ee2e366dd1989fc4baf437bc150f6a5cd
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2011-11-19 22:32:05 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Communism::Application.config.secret_token = '32237e2470535249dde764fe201f207eed881cd2f497f3f4653fad420a4c77e881f3131b96444e2dc2642a88679ff7b7f97eea00029de0eaa4426136a702d36e'
